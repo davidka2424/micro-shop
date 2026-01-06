6d28c2ae4f78e00bb8a6cc166298b4be40c5a4cf
David Shamoev <shamoevdavid04@gmail.com>
2026-01-06 17:04:45 +0500

    .

--- users/schemas.py
+++ users/schemas.py
@@ -4,5 +4,5 @@ from annotated_types import MinLen, MaxLen
 
 
 class CreateUser(BaseModel):
-    username: Annotated[str, MinLen(3), MaxLen(20)] 
+    username: Annotated[str, MinLen(3), MaxLen(20)]
     email: EmailStr
